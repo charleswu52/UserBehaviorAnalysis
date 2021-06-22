8fa41dd471b06f0804c58902b4b5530e1b5d01ee
charleswu52 <charleswu5201@gmail.com>
2021-06-22 16:47:35 +0800

    update README.md and PageView

--- NetworkFlowAnalysis/src/main/java/networkflow_analysis/hotPages/PageView.java
+++ NetworkFlowAnalysis/src/main/java/networkflow_analysis/hotPages/PageView.java
@@ -17,7 +17,6 @@ import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
-import org.apache.flink.table.planner.expressions.In;
 import org.apache.flink.util.Collector;
 
 import java.net.URL;
